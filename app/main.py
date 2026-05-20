b189a55f130a832623d06cd70c18eb30e6acbd1f
GuDLish <parmar54sai@gmail.com>
2026-05-20 17:33:51 +0530

    Updated FastAPI service

--- app/main.py
+++ app/main.py
@@ -3,42 +3,99 @@ from pydantic import BaseModel
 import pickle
 import numpy as np
 
-app = FastAPI()
+
 
 with open("model.pkl", "rb") as file:
+
     model = pickle.load(file)
 
+
+app = FastAPI()
+
+
 class CustomerData(BaseModel):
-    total_orders: int
-    total_spent: float
-    avg_order_value: float
-    avg_delivery_days: float
-    return_rate: float
-    avg_rating: float
+
+    recency_days: int
+
+    frequency_180d: int
+
+    monetary_180d: float
+
+    return_rate_180d: float
+
+    avg_discount_pct_180d: float
+
+    avg_rating_180d: float
+
+    category_diversity_180d: int
+
+    ticket_count_90d: int
+
+    negative_ticket_rate_90d: float
+
+    avg_resolution_hours_90d: float
+
+    days_since_signup: int
+
+    sessions_30d: int
+
+
 
 @app.get("/")
+
 def home():
+
     return {
-        "message": "D2C Customer Churn Prediction API"
+
+        "message": "D2C Churn Prediction API is running successfully"
+
     }
 
+
 @app.post("/predict")
+
 def predict(data: CustomerData):
 
-    features = np.array([[
-        data.total_orders,
-        data.total_spent,
-        data.avg_order_value,
-        data.avg_delivery_days,
-        data.return_rate,
-        data.avg_rating
-    ]])
+    features = [[
+
+        data.recency_days,
+
+        data.frequency_180d,
+
+        data.monetary_180d,
+
+        data.return_rate_180d,
+
+        data.avg_discount_pct_180d,
+
+        data.avg_rating_180d,
+
+        data.category_diversity_180d,
+
+        data.ticket_count_90d,
+
+        data.negative_ticket_rate_90d,
+
+        data.avg_resolution_hours_90d,
+
+        data.days_since_signup,
+
+        data.sessions_30d
+
+    ]]
+
+
 
     prediction = model.predict(features)[0]
 
     probability = model.predict_proba(features)[0][1]
 
+
+
     return {
+
         "prediction": int(prediction),
+
         "churn_probability": float(probability)
+
     }
--- test_api.py
+++ test_api.py
@@ -3,17 +3,39 @@ import requests
 url = "http://127.0.0.1:8000/predict"
 
 data = {
-    "total_orders": 5,
-    "total_spent": 3200,
-    "avg_order_value": 640,
-    "avg_delivery_days": 4,
-    "return_rate": 0.1,
-    "avg_rating": 4.2
+
+    "recency_days": 20,
+
+    "frequency_180d": 12,
+
+    "monetary_180d": 5000,
+
+    "return_rate_180d": 0.1,
+
+    "avg_discount_pct_180d": 15,
+
+    "avg_rating_180d": 4.2,
+
+    "category_diversity_180d": 5,
+
+    "ticket_count_90d": 2,
+
+    "negative_ticket_rate_90d": 0.2,
+
+    "avg_resolution_hours_90d": 12,
+
+    "days_since_signup": 400,
+
+    "sessions_30d": 18
+
 }
 
 response = requests.post(
+
     url,
+
     json=data
+
 )
 
 print(response.json())
